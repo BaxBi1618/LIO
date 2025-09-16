5f1c56f8160bd29e9684d3a67668c53f7d8553f1
Michał Garnys <m.programista@wp.pl>
2025-09-16 17:48:05 +0200

    hotfix

--- app/components/logged_user.php
+++ app/components/logged_user.php
@@ -2,19 +2,19 @@
     <a href="/expenses">
         <section class="titles">
             <img src="/images/expenses.png" alt="Wydatki">
-            <span class="highlight">Wydatki</span>
+            <span>Wydatki</span>
         </section>
     </a>
     <a href="checklists.php">
         <section class="titles">
             <img src="/images/checklist.png" alt="Listy">
-            <span class="highlight">Listy</span>
+            <span>Listy</span>
         </section>
     </a>
     <a href="checklists.php">
     <section class="titles">
         <img src="/images/calendar.png" alt="Kalendarz">
-        <span class="highlight">Kalendarz</span>
+        <span>Kalendarz</span>
     </section>
     </a>
     <a href="checklists.php">
@@ -26,7 +26,7 @@
     <a href="/feedback">
         <section class="titles">
             <img src="/images/feedback.png" alt="Notatki">
-            <span class="highlight">Feedback</span>
+            <span>Feedback</span>
         </section>
     </a>
 </main>
